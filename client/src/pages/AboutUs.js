1ff5e499315202607d2eb96f72e3ca320178716d
leomellbergholm <62606313+leomellbergholm@users.noreply.github.com>
2022-01-03 11:18:34 +0100

    aboutus

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, CssBaseline } from "@mui/material";
+import { Box, ThemeProvider } from "@mui/system";
+import Navbar from "./components/Navbar";
 
-const AboutUs = () => {
-  return <div></div>;
-};
+import { getUser } from "./api";
+
+import PurpleBox from "./components/PurpleBox";
+import theme from "./theme";
+
+function AboutUs() {
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedUser = await getUser();
+
+      setUser(fetchedUser.data.username);
+    }
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <Navbar user={user} />
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <PurpleBox text={`Homepage`} />
+      </ThemeProvider>
+      <Container></Container>
+    </div>
+  );
+}
 
 export default AboutUs;
